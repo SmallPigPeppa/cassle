9b1eb0807cda5ea3df711c69a70fddf28938874d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-09 21:54:38 +0800

    change num tasks

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -26,7 +26,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100-modified \
-    --project cassle-fintunning \
+    --project cassle-finetuning \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

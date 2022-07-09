6ccfd9f1f29eae67812769cb69faa749560265f3
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-09 21:39:42 +0800

    debug

--- models/resnet.py
+++ models/resnet.py
@@ -24,8 +24,10 @@ model_urls = {
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
-    return Conv3x3_mofied(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+    # return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+    #                  padding=dilation, groups=groups, bias=False, dilation=dilation)
+    return Conv3x3_mofied(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation)
+
 
 
 def conv1x1(in_planes, out_planes, stride=1):

3d0f99637f5ca2a8b47ba2992eb7f49ae010e1a6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-09 21:13:23 +0800

    change conv3x3

--- cassle/methods/base.py
+++ cassle/methods/base.py
@@ -159,6 +159,7 @@ class BaseModel(pl.LightningModule):
 
         assert encoder in ["resnet18", "resnet50"]
         from torchvision.models import resnet18, resnet50
+        from models.resnet import resnet18, resnet50
 
         self.base_model = {"resnet18": resnet18, "resnet50": resnet50}[encoder]
 

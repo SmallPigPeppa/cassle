1d45b987fd83449408316558a660b08868ab523c
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-10 11:59:24 +0800

    test re_paramaterize

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -26,7 +26,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100-modified \
-    --project cassle-finetuning \
+    --project cassle-debug \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

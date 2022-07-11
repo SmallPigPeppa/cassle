dfbdff39e3561552ae91f6acf55eff6f0028a949
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-11 14:40:29 +0800

    debug  v2.0

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -25,8 +25,8 @@ python3 main_continual.py \
     --saturation 0.8 \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
-    --name simclr-cifar100-modifiedv2 \
-    --project cassle-debug \
+    --name simclr-cifar100 \
+    --project cassle-modifiedv2 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

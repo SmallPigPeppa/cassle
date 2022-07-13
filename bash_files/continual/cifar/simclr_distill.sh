54a011db324992e7c0dbc1d1b09658302cf611ff
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-13 11:00:07 +0800

    v14.0 initial

--- bash_files/continual/cifar/simclr_distill.sh
+++ bash_files/continual/cifar/simclr_distill.sh
@@ -26,7 +26,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100-contrastive \
-    --project casslev11 \
+    --project casslev14 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \
--- main_continual.py
+++ main_continual.py
@@ -84,10 +84,10 @@ if __name__ == "__main__":
 
         # add use_expansion and re_reparameterize
 
-        if task_idx in [1,3]:
+        if task_idx in [1,3,4]:
             task_args['--use_expansion'] = '   '
         # use re_paramaterize after task1
-        if task_idx in [2,4]:
+        if task_idx in [2]:
             task_args['--re_paramaterize'] = '   '
 
         # if task_idx == 1 :

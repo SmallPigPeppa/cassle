b7c039ebf401c1f7c57f21093914637c62c42e69
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-13 20:59:44 +0800

    initial bnv2

--- models/conv_modified.py
+++ models/conv_modified.py
@@ -1,16 +1,33 @@
 import torch.nn as nn
 import torch
 import numpy as np
+def conv_bn(in_channels, out_channels, kernel_size, stride, padding, groups=1):
+    result = nn.Sequential()
+    result.add_module('conv', nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                                  kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False))
+    result.add_module('bn', nn.BatchNorm2d(num_features=out_channels))
+    return result
 
-
-class Conv3x3_mofied(nn.Module):
+class Conv3x3_modified(nn.Module):
+    """
+    add bn into conv
+    self.expansion_1x1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
+    self.expansion_1x1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    self.expansion_3x3 = conv3x3(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation)
+    self.expansion_3x3.weight.data.zero_()
+    """
     def __init__(self, in_planes, out_planes, stride=1, groups=1, dilation=1,use_expansion=False):
-        super(Conv3x3_mofied, self).__init__()
-        # nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-        self.conv2d_3x3 = conv3x3(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation)
-        # self.expansion_1x1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-        self.expansion_3x3 = conv3x3(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation)
-        self.expansion_3x3.weight.data.zero_()
+        super(Conv3x3_modified, self).__init__()
+
+        self.conv2d_3x3=nn.Sequential()
+        self.conv2d_3x3.add_module('conv',conv3x3(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation))
+        self.conv2d_3x3.add_module('bn', nn.BatchNorm2d(out_planes))
+
+        self.expansion_3x3=nn.Sequential()
+        self.expansion_3x3.add_module('conv',conv3x3(in_planes, out_planes, stride=stride, groups=groups, dilation=dilation))
+        self.expansion_3x3.add_module('bn', nn.BatchNorm2d(out_planes))
+
         self.use_expansion = use_expansion
 
     def forward(self, x):
@@ -77,60 +94,15 @@ class Conv3x3_mofied(nn.Module):
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+                     padding=dilation, groups=groups, bias=True, dilation=dilation)
 
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=True)
 
 
 def conv3x3_modified(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    return Conv3x3_mofied(in_planes, out_planes, stride=stride,
+    return Conv3x3_modified(in_planes, out_planes, stride=stride,
                            groups=groups,dilation=dilation)
 
-def conv3x3_modified_padding(in_planes, out_planes, stride=1, groups=1, dilation=1,padding=1):
-    conv_m=Conv3x3_mofied(in_planes, out_planes, stride=stride,
-                           groups=groups,dilation=dilation)
-    conv_m.conv2d_3x3=nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=padding, groups=groups, bias=False, dilation=dilation)
-    return conv_m
-
-class BasicBlock_Modified(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(BasicBlock_Modified, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out

4837b6c2a96daf4548ad1983c2a61df107bcee71
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-15 14:42:38 +0800

    initial v18.0

--- models/conv_modified.py
+++ models/conv_modified.py
@@ -15,8 +15,7 @@ class Conv3x3_mofied(nn.Module):
         if not self.use_expansion:
             return self.conv2d_3x3(x)
         else:
-            with torch.no_grad():
-                out1 = self.conv2d_3x3(x)
+            out1 = self.conv2d_3x3(x)
             return self.expansion_1x1(x) + out1
 
     def set_expansion(self, use_expansion=True):

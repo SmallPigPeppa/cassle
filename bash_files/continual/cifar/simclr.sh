8d97652d5012f6510da07faa8af49835fd3bceb9
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-18 19:15:16 +0800

    initial v26.0

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -4,9 +4,9 @@ python3 main_continual.py \
     --data_dir $DATA_DIR \
     --split_strategy class \
     --task_idx 0 \
-    --max_epochs 500 \
+    --max_epochs 1 \
     --num_tasks 5 \
-    --max_epochs 500 \
+    --max_epochs 1 \
     --gpus 0 \
     --precision 16 \
     --optimizer sgd \
@@ -26,7 +26,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100 \
-    --project casslev24 \
+    --project casslev26 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \
--- cassle/distillers/contrastive.py
+++ cassle/distillers/contrastive.py
@@ -1,11 +1,10 @@
 import argparse
-from typing import Any, List, Sequence
-
+from typing import Any, Callable, Dict, List, Sequence, Tuple
 import torch
 from torch import nn
 from cassle.distillers.base import base_distill_wrapper
 from cassle.losses.simclr import simclr_distill_loss_func
-
+from cassle.utils.momentum import MomentumUpdater, initialize_momentum_params
 
 def contrastive_distill_wrapper(Method=object):
     class ContrastiveDistillWrapper(base_distill_wrapper(Method)):
@@ -21,14 +20,14 @@ def contrastive_distill_wrapper(Method=object):
             self.distill_lamb = distill_lamb
             self.distill_temperature = distill_temperature
             output_dim = kwargs["output_dim"]
-
             self.distill_predictor = nn.Sequential(
                 nn.Linear(output_dim, distill_proj_hidden_dim),
                 nn.BatchNorm1d(distill_proj_hidden_dim),
                 nn.ReLU(),
                 nn.Linear(distill_proj_hidden_dim, output_dim),
             )
-
+            # self.momentum_updater = MomentumUpdater(base_tau_momentum, final_tau_momentum)
+            self.momentum_updater = MomentumUpdater(0.99, 1.0)
         @staticmethod
         def add_model_specific_args(
             parent_parser: argparse.ArgumentParser,
@@ -70,5 +69,38 @@ def contrastive_distill_wrapper(Method=object):
             self.log("train_contrastive_distill_loss", distill_loss, on_epoch=True, sync_dist=True)
 
             return out["loss"] + self.distill_lamb * distill_loss
+
+        def on_train_batch_end(self, outputs: Dict[str, Any], batch: Sequence[Any], batch_idx: int):
+            """Performs the momentum update of momentum pairs using exponential moving average at the
+            end of the current training step if an optimizer step was performed.
+
+            Args:
+                outputs (Dict[str, Any]): the outputs of the training step.
+                batch (Sequence[Any]): a batch of data in the format of [img_indexes, [X], Y], where
+                    [X] is a list of size self.num_crops containing batches of images.
+                batch_idx (int): index of the batch.
+            """
+
+            if self.trainer.global_step > self.last_step:
+                # update momentum encoder and projector
+                # self.frozen_encoder = deepcopy(self.encoder)
+                # self.frozen_projector = deepcopy(self.projector)
+                # [(self.encoder, self.momentum_encoder)]
+                momentum_pairs = [(self.encoder, self.frozen_encoder),(self.projector,self.frozen_projector)]
+                for mp in momentum_pairs:
+                    self.momentum_updater.update(*mp)
+                # log tau momentum
+                self.log("tau", self.momentum_updater.cur_tau)
+                # update tau
+                self.momentum_updater.update_tau(
+                    cur_step=self.trainer.global_step * self.trainer.accumulate_grad_batches,
+                    max_steps=len(self.trainer.train_dataloader) * self.trainer.max_epochs,
+                )
+            self.last_step = self.trainer.global_step
+
+        def on_train_start(self):
+            """Resets the step counter at the beginning of training."""
+            super().on_train_start()
+            self.last_step = 0
 
     return ContrastiveDistillWrapper

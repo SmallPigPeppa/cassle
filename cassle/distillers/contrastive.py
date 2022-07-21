295daf0fc5a8b628c93d78e436362b1e2f869b6e
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-21 22:27:34 +0800

    initial v37.0

--- cassle/distillers/contrastive.py
+++ cassle/distillers/contrastive.py
@@ -117,7 +117,7 @@ def contrastive_distill_wrapper(Method=object):
             # p2=p2[valid_mask]
             # frozen_z1=frozen_z1[valid_mask]
             # frozen_z2 = frozen_z2[valid_mask]
-            self.log("valid_sample", sum(valid_mask), on_epoch=True, sync_dist=True)
+            self.log("valid_sample", (valid_mask.sum()), on_epoch=True, sync_dist=True)
             if 1 >0:
                 distill_loss = (
                                        simclr_distill_loss_func(p1, p2, frozen_z1, frozen_z2, self.distill_temperature,valid_pos=valid_mask)

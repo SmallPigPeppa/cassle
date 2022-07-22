4823747775aa31523feb44f92fa34273f4358f21
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-23 00:29:41 +0800

    initial

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def get_modified_state_dict(state_dict):
     modified_resnet=resnet18()
     modified_resnet_keys=[i for i,_ in modified_resnet.named_parameters()]
     for key, value in state_dict.items():
-        if key not in modified_resnet_keys and 'conv2' in key and 'encoder' in key:
+        if key not in modified_resnet_keys and 'conv2' in key and 'encoder' in key and 'frozen' not in key:
             a = key.split('.')
             key = f'{a[0]}.{a[1]}.{a[2]}.{a[3]}.conv2d_3x3.{a[4]}'
         modified_state_dict[key] = value

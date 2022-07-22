80b7cd4325e7d7316d592d554ceb70c6030fa596
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-22 23:56:36 +0800

    initial

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-from models.resnet_modified import resnet50
+from models.resnet_modified import resnet18
 def get_modified_state_dict(state_dict):
     import collections
     modified_state_dict = collections.OrderedDict()
-    modified_resnet=resnet50()
+    modified_resnet=resnet18()
     modified_resnet_keys=[i for i,_ in modified_resnet.named_parameters()]
     for key, value in state_dict.items():
         if key not in modified_resnet_keys and 'conv2' in key:

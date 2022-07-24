cb862482e5b92476e4908c4600d01a426aa8ace1
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-24 18:38:38 +0800

    initial

--- cassle/methods/base.py
+++ cassle/methods/base.py
@@ -314,9 +314,9 @@ class BaseModel(pl.LightningModule):
                 # no_wd_params.append(param)
             print(len(wd_params), len(no_wd_params), len(all_params))
             assert len(wd_params) + len(no_wd_params) == len(all_params), "Sanity check failed."
+            # {"name": "encoder_no_wd_params", "params": no_wd_params, "weight_decay": 0, },
             return [
                 {"name": "encoder", "params": wd_params, "weight_decay": self.weight_decay, },
-                {"name": "encoder_no_wd_params", "params": no_wd_params, "weight_decay": 0, },
                 {
                     "name": "classifier",
                     "params": self.classifier.parameters(),

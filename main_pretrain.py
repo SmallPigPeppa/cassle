d9a77c60bb6a077edbe9dd4e9f4aed09fb8d3170
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-25 13:22:08 +0800

    initial

--- main_pretrain.py
+++ main_pretrain.py
@@ -247,7 +247,9 @@ def main():
     model.current_task_idx = args.task_idx
     # 保存一下初始化
     initial_ckpt_path='/admin/code/cassle_initial.ckpt'
-    trainer.save_checkpoint(initial_ckpt_path)
+    trainer_tmp=Trainer(max_epochs=0)
+    trainer_tmp.fit(model,train_loaders, val_loader)
+    trainer_tmp.save_checkpoint(initial_ckpt_path)
     print('************************************* save initial ckpt ********************************')
     print(f'initial_ckpt_path:{initial_ckpt_path}')
 

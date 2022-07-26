9aa73d0d6d8fe1e03a499ff62d14ae856e9c184d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-26 22:47:42 +0800

    initial

--- main_pretrain.py
+++ main_pretrain.py
@@ -163,7 +163,7 @@ def main():
     elif args.pretrained_model:
         print(f"Loading previous task checkpoint {args.pretrained_model}...")
         state_dict = torch.load(args.pretrained_model, map_location="cpu")["state_dict"]
-        if args.task_idx == 0:
+        if args.task_idx == 1 and not args.fixed_model_path:
             from utils import get_modified_state_dict
             state_dict = get_modified_state_dict(state_dict)
         model.load_state_dict(state_dict, strict=False)

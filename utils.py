90351d1c919898ab9af9a0d524407df363c15f2a
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-26 22:26:57 +0800

    initial

--- utils.py
+++ utils.py
@@ -4,11 +4,19 @@ def get_modified_state_dict(state_dict):
     modified_state_dict = collections.OrderedDict()
     # modified_resnet=resnet18()
     # modified_resnet_keys=[i for i,_ in modified_resnet.named_parameters()]
-    for key, value in state_dict.items():
-        if key not in modified_resnet_keys and 'conv' in key and 'encoder' in key and 'layer' in key:
+    # for key, value in state_dict.items():
+    #     if 'conv' in key and 'encoder' in key and 'layer' in key:
+    #         a = key.split('.')
+    #         key = f'{a[0]}.{a[1]}.{a[2]}.{a[3]}.conv2d_3x3.{a[4]}'
+    #     modified_state_dict[key] = value
+    # return modified_state_dict
 
+    for key, value in state_dict.items():
+        if 'conv' in key and 'encoder' in key and 'layer' in key:
+            print(key)
             a = key.split('.')
-            key = f'{a[0]}.{a[1]}.{a[2]}.{a[3]}.conv2d_3x3.{a[4]}'
+            key2 = f'{a[0]}.{a[1]}.{a[2]}.{a[3]}.conv2d_3x3.{a[4]}'
+            print(key2)
         modified_state_dict[key] = value
     return modified_state_dict
 
@@ -17,7 +25,11 @@ def get_modified_state_dict(state_dict):
 
 
 if __name__=='__main__':
-    import torch.nn as nn
-    a=nn.Conv2d(3, 3, kernel_size=1, stride=1, bias=False)
-    a.zero_grad()
-    print(a.weight.grad)
+    # import torch.nn as nn
+    # a=nn.Conv2d(3, 3, kernel_size=1, stride=1, bias=False)
+    # a.zero_grad()
+    # print(a.weight.grad)
+    import torch
+    PRETRAINED_PATH='/home/admin/code/cassle_official/experiments/2022_07_13_17_37_12-simclr-cifar100/2cpkm0r5/simclr-cifar100-task0-ep=499-2cpkm0r5.ckpt'
+    state_dict = torch.load(PRETRAINED_PATH, map_location="cpu")["state_dict"]
+    get_modified_state_dict(state_dict=state_dict)

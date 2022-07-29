1dc805cf0e2f66cc8a97389468b97403efa5ac29
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-29 23:14:06 +0800

    initial

--- bash_files/continual/cifar/simclr_distill.sh
+++ bash_files/continual/cifar/simclr_distill.sh
@@ -4,9 +4,9 @@ python3 main_continual.py \
     --data_dir $DATA_DIR \
     --split_strategy class \
     --task_idx 0 \
-    --max_epochs 1 \
+    --max_epochs 500 \
     --num_tasks 10 \
-    --max_epochs 1 \
+    --max_epochs 500 \
     --gpus 0 \
     --precision 16 \
     --optimizer sgd \

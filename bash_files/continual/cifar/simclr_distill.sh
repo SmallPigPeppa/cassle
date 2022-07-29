a6e4821e9ec4337ca15e8a99a69f5051a51e669d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-29 21:34:04 +0800

    initial

--- bash_files/continual/cifar/simclr_distill.sh
+++ bash_files/continual/cifar/simclr_distill.sh
@@ -26,7 +26,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100-v61 \
-    --project cassle-v50 \
+    --project cassle-v51 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

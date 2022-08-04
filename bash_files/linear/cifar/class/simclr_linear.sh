7cd4ce1ab80e21d1221ee5ae2f60e8826ab43158
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-04 12:33:08 +0800

    initial

---
+++ bash_files/linear/cifar/class/simclr_linear.sh
@@ -0,0 +1,23 @@
+python3 main_linear.py \
+    --dataset cifar100 \
+    --encoder resnet18 \
+    --data_dir $DATA_DIR \
+    --split_strategy class \
+    --num_tasks 5 \
+    --max_epochs 100 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --scheduler step \
+    --lr 1.0 \
+    --lr_decay_steps 60 80 \
+    --weight_decay 0 \
+    --batch_size 256 \
+    --num_workers 7 \
+    --dali \
+    --name v51 \
+    --pretrained_feature_extractor $PRETRAINED_PATH \
+    --project cassle-eval \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint

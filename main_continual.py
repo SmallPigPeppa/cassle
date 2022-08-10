3fc508a885d90ac3c37bcadbd06ecf14d28eaa12
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-11 00:16:08 +0800

    initial

--- main_continual.py
+++ main_continual.py
@@ -84,11 +84,11 @@ if __name__ == "__main__":
 
         # add use_expansion and re_reparameterize
 
-        if task_idx != 0 and task_idx != start_task_idx:
+        if task_idx in [1,3,4]:
             task_args['--use_expansion'] = '   '
 
         # use re_paramaterize after task1
-        if task_idx > 1 and task_idx - start_task_idx > 1:
+        if task_idx in [2]:
             task_args['--re_paramaterize'] = '   '
 
         # if task_idx == 1 :

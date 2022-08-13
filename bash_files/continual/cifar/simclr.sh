4ee49a72fe4ae65f9cddc9d6947c783ae7b6067d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-13 12:54:36 +0800

    initial

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -5,7 +5,7 @@ python3 main_continual.py \
     --split_strategy class \
     --task_idx 0 \
     --num_tasks 10 \
-    --max_epochs 250 \
+    --max_epochs 500 \
     --gpus 0 \
     --precision 16 \
     --optimizer sgd \
@@ -25,7 +25,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100 \
-    --project casslev126 \
+    --project casslev131 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

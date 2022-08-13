5fd6533ef5afbeaaa120d08f2fda127d4575befe
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-13 20:56:43 +0800

    initial

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -25,7 +25,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100 \
-    --project casslev135 \
+    --project casslev136 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

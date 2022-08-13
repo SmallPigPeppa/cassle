9d070ea664a9a8c49aac94caff00ef4555602769
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-13 19:41:52 +0800

    initial

--- bash_files/continual/cifar/simclr_distill.sh
+++ bash_files/continual/cifar/simclr_distill.sh
@@ -4,7 +4,7 @@ python3 main_continual.py \
     --data_dir $DATA_DIR \
     --split_strategy class \
     --task_idx 0 \
-    --max_epochs 500 \
+    --max_epochs 250 \
     --num_tasks 10 \
     --gpus 0 \
     --precision 16 \
@@ -25,7 +25,7 @@ python3 main_continual.py \
     --hue 0.2 \
     --gaussian_prob 0.0 0.0 \
     --name simclr-cifar100-contrastive \
-    --project casslev129 \
+    --project casslev134 \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \

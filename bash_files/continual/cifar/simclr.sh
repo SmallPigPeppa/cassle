6c97095c4d950d7061d43a2e623f0213aaf1d255
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-13 19:50:09 +0800

    initial

--- bash_files/continual/cifar/simclr.sh
+++ bash_files/continual/cifar/simclr.sh
@@ -4,8 +4,8 @@ python3 main_continual.py \
     --data_dir $DATA_DIR \
     --split_strategy class \
     --task_idx 0 \
-    --max_epochs 500 \
-    --num_tasks 5 \
+    --max_epochs 250 \
+    --num_tasks 10 \
     --gpus 0 \
     --precision 16 \
     --optimizer sgd \

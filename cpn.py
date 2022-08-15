5d57f392bb8920c00778ca7bdcc0fbf50011b7e2
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-15 22:23:12 +0800

    initial

--- cpn.py
+++ cpn.py
@@ -1,23 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
 class PrototypeClassifier(nn.Module):
     def __init__(self, dim_features, num_classes, centers=None):
         super().__init__()
         self.dim_features = dim_features
         self.num_calsses = num_classes
         if centers is not None:
-            self.prototypes = nn.Parameter(torch.tensor(centers,dtype=float))
+            self.prototypes = nn.Parameter(torch.tensor(centers, dtype=float))
         else:
             self.prototypes = nn.Parameter(torch.randn(self.num_calsses, self.dim_features))
 
     def forward(self, x):
         x = x.reshape(-1, 1, self.dim_features)
-        d = torch.pow(x - self.prototypes, 2)
+        # d = torch.pow(x - self.prototypes, 2)
+        d = torch.absolute(x - self.prototypes)
         d = torch.sum(d, dim=2)
         return d
 
-    def logits(self,x):
-        d=self.forward(x)
-        logits=F.softmax(-10.*d,dim=1)
+    def logits(self, x):
+        d = self.forward(x)
+        logits = F.softmax(-10. * d, dim=1)
         return logits

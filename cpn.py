c75631a5e36b87337acbc19b871a43524abd0ef7
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-17 01:03:57 +0800

    initial

--- cpn.py
+++ cpn.py
@@ -3,6 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# class PrototypeClassifier(nn.Module):
+#     def __init__(self, dim_features, num_classes, centers=None):
+#         super().__init__()
+#         self.cosine=nn.CosineSimilarity(dim=2, eps=1e-6)
+#         self.dim_features = dim_features
+#         self.num_calsses = num_classes
+#         if centers is not None:
+#             self.prototypes = nn.Parameter(torch.tensor(centers, dtype=float))
+#         else:
+#             self.prototypes = nn.Parameter(torch.randn(self.num_calsses, self.dim_features))
+#
+#
+#     def forward(self, x):
+#         x = x.reshape(-1, 1, self.dim_features)
+#         prototypes=self.prototypes.reshape(1,-1,self.dim_features)
+#         cos=self.cosine(x,prototypes)
+#         return cos
+#         # d = torch.pow(x - self.prototypes, 2)
+#         # d = torch.absolute(x - self.prototypes)
+#         # d = torch.sum(d, dim=2)
+#         # return d
+#
+#     def logits(self, x):
+#         d = self.forward(x)
+#         logits = F.softmax(10. * d, dim=1)
+#         return logits
+
+
 class PrototypeClassifier(nn.Module):
     def __init__(self, dim_features, num_classes, centers=None):
         super().__init__()
@@ -17,15 +45,15 @@ class PrototypeClassifier(nn.Module):
 
     def forward(self, x):
         x = x.reshape(-1, 1, self.dim_features)
-        prototypes=self.prototypes.reshape(1,-1,self.dim_features)
-        cos=self.cosine(x,prototypes)
-        return cos
-        # d = torch.pow(x - self.prototypes, 2)
-        # d = torch.absolute(x - self.prototypes)
-        # d = torch.sum(d, dim=2)
-        # return d
+        # prototypes=self.prototypes.reshape(1,-1,self.dim_features)
+        # cos=self.cosine(x,prototypes)
+        # return cos
+        d = torch.pow(x - self.prototypes, 2)
+        d = torch.absolute(x - self.prototypes)
+        d = torch.sum(d, dim=2)
+        return d
 
     def logits(self, x):
         d = self.forward(x)
-        logits = F.softmax(10. * d, dim=1)
+        logits = F.softmax(-10. * d, dim=1)
         return logits
---
+++ vis_kmeans_pretrain_distance.py
@@ -0,0 +1,289 @@
+import os
+from pprint import pprint
+import types
+
+import torch
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
+
+from cassle.args.setup import parse_args_pretrain
+from cassle.methods import METHODS
+from cassle.distillers import DISTILLERS
+
+try:
+    from cassle.methods.dali import PretrainABC
+except ImportError:
+    _dali_avaliable = False
+else:
+    _dali_avaliable = True
+
+try:
+    from cassle.utils.auto_umap import AutoUMAP
+except ImportError:
+    _umap_available = False
+else:
+    _umap_available = True
+
+from cassle.utils.checkpointer import Checkpointer
+from cassle.utils.classification_dataloader import prepare_data as prepare_data_classification
+from cassle.utils.pretrain_dataloader import (
+    prepare_dataloader,
+    prepare_datasets,
+    prepare_multicrop_transform,
+    prepare_n_crop_transform,
+    prepare_transform,
+    split_dataset,
+)
+
+
+def main():
+    seed_everything(5)
+
+    args = parse_args_pretrain()
+
+    # online eval dataset reloads when task dataset is over
+    args.multiple_trainloader_mode = "min_size"
+
+    # set online eval batch size and num workers
+    args.online_eval_batch_size = int(args.batch_size) if args.dataset == "cifar100" else None
+
+    # split classes into tasks
+    tasks = None
+    if args.split_strategy == "class":
+        assert args.num_classes % args.num_tasks == 0
+        tasks = torch.tensor(list(range(args.num_classes))).chunk(args.num_tasks)
+
+    # pretrain and online eval dataloaders
+    if not args.dali:
+        # asymmetric augmentations
+        if args.unique_augs > 1:
+            transform = [
+                prepare_transform(args.dataset, multicrop=args.multicrop, **kwargs)
+                for kwargs in args.transform_kwargs
+            ]
+        else:
+            transform = prepare_transform(
+                args.dataset, multicrop=args.multicrop, **args.transform_kwargs
+            )
+
+        if args.debug_augmentations:
+            print("Transforms:")
+            pprint(transform)
+
+        if args.multicrop:
+            assert not args.unique_augs == 1
+
+            if args.dataset in ["cifar10", "cifar100"]:
+                size_crops = [32, 24]
+            elif args.dataset == "stl10":
+                size_crops = [96, 58]
+            # imagenet or custom dataset
+            else:
+                size_crops = [224, 96]
+
+            transform = prepare_multicrop_transform(
+                transform, size_crops=size_crops, num_crops=[args.num_crops, args.num_small_crops]
+            )
+        else:
+            if args.num_crops != 2:
+                assert args.method == "wmse"
+
+            online_eval_transform = transform[-1] if isinstance(transform, list) else transform
+            task_transform = prepare_n_crop_transform(transform, num_crops=args.num_crops)
+
+        train_dataset, online_eval_dataset = prepare_datasets(
+            args.dataset,
+            task_transform=task_transform,
+            online_eval_transform=online_eval_transform,
+            data_dir=args.data_dir,
+            train_dir=args.train_dir,
+            no_labels=args.no_labels,
+        )
+
+        task_dataset, tasks = split_dataset(
+            train_dataset,
+            tasks=tasks,
+            task_idx=args.task_idx,
+            num_tasks=args.num_tasks,
+            split_strategy=args.split_strategy,
+        )
+
+        task_loader = prepare_dataloader(
+            task_dataset,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+        )
+
+        train_loaders = {f"task{args.task_idx}": task_loader}
+
+        if args.online_eval_batch_size:
+            online_eval_loader = prepare_dataloader(
+                online_eval_dataset,
+                batch_size=args.online_eval_batch_size,
+                num_workers=args.num_workers,
+            )
+            train_loaders.update({"online_eval": online_eval_loader})
+
+    # normal dataloader for when it is available
+    if args.dataset == "custom" and (args.no_labels or args.val_dir is None):
+        val_loader = None
+    elif args.dataset in ["imagenet100", "imagenet"] and args.val_dir is None:
+        val_loader = None
+    else:
+        _, val_loader = prepare_data_classification(
+            args.dataset,
+            data_dir=args.data_dir,
+            train_dir=args.train_dir,
+            val_dir=args.val_dir,
+            batch_size=args.batch_size,
+            num_workers=2 * args.num_workers,
+        )
+
+    # check method
+    assert args.method in METHODS, f"Choose from {METHODS.keys()}"
+    # build method
+    MethodClass = METHODS[args.method]
+    if args.dali:
+        assert (
+            _dali_avaliable
+        ), "Dali is not currently avaiable, please install it first with [dali]."
+        MethodClass = types.new_class(f"Dali{MethodClass.__name__}", (PretrainABC, MethodClass))
+
+    if args.distiller:
+        MethodClass = DISTILLERS[args.distiller](MethodClass)
+
+    model = MethodClass(**args.__dict__, tasks=tasks if args.split_strategy == "class" else None)
+
+    # only one resume mode can be true
+    assert [args.resume_from_checkpoint, args.pretrained_model].count(True) <= 1
+
+    if args.resume_from_checkpoint:
+        pass  # handled by the trainer
+    elif args.pretrained_model:
+        print(f"Loading previous task checkpoint {args.pretrained_model}...")
+        state_dict = torch.load(args.pretrained_model, map_location="cpu")["state_dict"]
+        model.load_state_dict(state_dict, strict=False)
+
+    # visualize feats on task1
+    # from tsne_torch import TorchTSNE as TSNE
+
+    feats_all = []
+    labels_all = []
+    from tqdm import tqdm
+    import numpy as np
+    for batch in tqdm(task_loader):
+        # for batch in tqdm(val_loader):
+        imgs = batch[1][0]
+        imgs2=batch[1][1]
+        labels = batch[2]
+        # imgs=batch[0]
+        # labels=batch[1]
+        # print(len(batch))
+        # print(imgs.shape)
+        # print(labels.shape)
+        out_i = model(imgs)
+        z_i = out_i["feats"]
+        # z_i = out_i["z"]
+        feats_all.append(z_i.cpu().detach().numpy())
+        labels_all.append(labels.cpu().detach().numpy())
+        out_i = model(imgs2)
+        z_i = out_i["feats"]
+        # z_i = out_i["z"]
+        feats_all.append(z_i.cpu().detach().numpy())
+        labels_all.append(labels.cpu().detach().numpy())
+        # break
+    feats_all = np.vstack(feats_all)
+    labels_all = np.hstack(labels_all)
+
+    print("feats_all.shape:", feats_all.shape)
+    print("labels_all.shape:", labels_all.shape)
+
+    # feats_emb = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=3).fit_transform(feats_all)
+    # # plot on each class
+    # import matplotlib.pyplot as plt
+    #
+    feats_all_kmeans = []
+    labels_all_kmeans = []
+    dim_features=512
+    for i in range(50, 55):
+        index_ci = np.where(labels_all == i)[0]
+        feats_ci = feats_all[index_ci]
+        feats_all_kmeans.append(feats_ci)
+        labels_all_kmeans.append([i] * len(feats_ci))
+    feats_all_kmeans = np.vstack(feats_all_kmeans)
+    labels_all_kmeans = np.hstack(labels_all_kmeans)
+
+    from sklearn import preprocessing
+    from sklearn.cluster import KMeans
+
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(feats_all_kmeans)
+    print(str([i for i in kmeans.labels_]))
+    print("len(kmeans.labels_):", len(kmeans.labels_))
+    dict = {'3': 50, '4': 51, '0': 52, '1': 53, '2':54}
+    right = 0
+    for i, x in enumerate(kmeans.labels_):
+        if labels_all_kmeans[i] == dict[str(x)]:
+            right += 1
+    print('acc:', right / len(kmeans.labels_))
+
+    from cpn import PrototypeClassifier
+    print("kmeans.cluster_centers_.shape:", kmeans.cluster_centers_.shape)
+    m_cpn = PrototypeClassifier(dim_features=dim_features, num_classes=5,
+                                centers=kmeans.cluster_centers_)
+    logits_all_kmeans = m_cpn.logits(torch.tensor(preprocessing.normalize(feats_all_kmeans)))
+    # logits_all_kmeans=logits_all_kmeans.cpu().detach().numpy()
+    print(str(logits_all_kmeans.cpu().detach().numpy()))
+    max_logits, _ = torch.max(logits_all_kmeans, 1)
+    max_logits = max_logits.cpu().detach().numpy()
+
+    # valid_rate = 0
+    # threshold = 1.0
+    # while valid_rate < 0.8:
+    #     valid_mask = np.where(max_logits >= threshold)[0]
+    #     print("threshold:", threshold)
+    #     print("valid_mask:", str(valid_mask))
+    #     print("len(max_logits):",len(max_logits))
+    #     print("valid_rate:", len(valid_mask) / len(max_logits))
+    #     print("##################################")
+    #     threshold = threshold - 0.05
+    #     valid_rate = len(valid_mask) / len(logits_all_kmeans)
+
+    valid_mask = np.where(max_logits >= 0.5)[0]
+    print("valid_rate:", len(valid_mask) / len(max_logits))
+    feats_all_kmeans = feats_all_kmeans[valid_mask]
+    labels_all_kmeans = labels_all_kmeans[valid_mask]
+    kmeans_labels = kmeans.labels_[valid_mask]
+    kmeans_labels = [dict[str(i)] for i in kmeans_labels]
+    idx_true = [idx for idx, element in enumerate(zip(kmeans_labels, labels_all_kmeans)) if element[0] == element[1]]
+    print("final acc after kmeans fliter:",len(idx_true)/len(kmeans_labels))
+    # feats_all_kmeans = feats_all_kmeans[idx_true]
+    # labels_all_kmeans = labels_all_kmeans[idx_true]
+
+    # #
+    # # print(feats_all2.shape,labels_all2.shape)
+    # feats_emb = TSNE(n_components=2, perplexity=30, n_iter=1000, verbose=True).fit_transform(
+    #     feats_all2)
+    from sklearn.manifold import TSNE
+    import matplotlib.pyplot as plt
+    feats_emb = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=3).fit_transform(feats_all_kmeans)
+
+    for i in tqdm(range(50, 55)):
+        index_ci = np.where(labels_all_kmeans == i)[0]
+        feats_emb_ci = feats_emb[index_ci]
+        # feats_ci_emb = TSNE(n_components=2, perplexity=30, n_iter=1000, verbose=True).fit_transform(
+        #     feats_ci)  # returns shape (n_samples, 2)
+        plt.scatter(feats_emb_ci[:, 0], feats_emb_ci[:, 1], label=f'class-{i}')
+
+    plt.legend()
+    # plt.show()
+    plt.savefig('/home/admin/result.pdf')
+    #
+    #
+    # # X = ...  # shape (n_samples, d)
+    # # X_emb = TSNE(n_components=2, perplexity=30, n_iter=1000, verbose=True).fit_transform(
+    # #     X)  # returns shape (n_samples, 2)
+
+
+if __name__ == "__main__":
+    main()

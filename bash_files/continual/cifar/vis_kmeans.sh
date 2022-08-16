677830016c1d216f92450a8e4d061032e974f308
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-17 00:52:46 +0800

    initial

--- bash_files/continual/cifar/vis_kmeans.sh
+++ bash_files/continual/cifar/vis_kmeans.sh
@@ -3,7 +3,7 @@ python3 vis_kmeans_pretrain.py \
     --encoder resnet18 \
     --data_dir $DATA_DIR \
     --split_strategy class \
-    --task_idx 2 \
+    --task_idx 1 \
     --max_epochs 500 \
     --num_tasks 5 \
     --gpus 0 \

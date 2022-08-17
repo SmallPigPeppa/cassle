9fc9a2707e8fc992956eed74c051051447e02b43
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-18 00:38:15 +0800

    initial

--- cassle/distillers/contrastive.py
+++ cassle/distillers/contrastive.py
@@ -111,14 +111,14 @@ def contrastive_distill_wrapper(Method=object):
             key_val = {key: val for key, val in zip(uniques, range(len(uniques)))}
             val_key = {val: key for key, val in zip(uniques, range(len(uniques)))}
 
-            labels = torch.LongTensor(list(map(key_val.get, labels)), device=self.device)
+            labels = torch.LongTensor(list(map(key_val.get, labels)))
 
             labels = labels.view(labels.size(0), 1).expand(-1, z.size(1))
 
             unique_labels, labels_count = labels.unique(dim=0, return_counts=True)
             result = torch.zeros_like(unique_labels, dtype=torch.float, device=self.device).scatter_add_(0, labels, z)
             result = result / labels_count.float().unsqueeze(1)
-            new_labels = torch.LongTensor(list(map(val_key.get, unique_labels[:, 0].tolist())),device=self.device)
+            new_labels = torch.LongTensor(list(map(val_key.get, unique_labels[:, 0].tolist())))
 
             _, order_index = new_labels.sort()
             return result[order_index]
